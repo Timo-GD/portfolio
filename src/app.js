8abb77fcfb91f0d52369d061543b74bc91c34a65
Timo-GD <omit400@gmail.com>
2025-09-29 23:39:35 +0200

    made a half working system for switching between images

--- src/app.js
+++ src/app.js
@@ -1,22 +1,70 @@
+let mainImage = 0;
+let sideImage = 5
 function switchImage(n) {
-    let mainImage = 0
     switch (n)
     {
         case 1:
-            document.getElementById("0").style.display = "none";
+            document.getElementById(mainImage).style.display = "none";
             document.getElementById("1").className = "main-image";
-            document.getElementById(mainImage).className = "side-image";
-            document.getElementById(mainImage).style.display = "block";
+            document.getElementById(sideImage).style.display = "";
+            sideImage = 6;
             mainImage = 1;
         break;
         case 2:
-            document.getElementById("2").className = "main-image"
+            document.getElementById(mainImage).style.display = "none";
+            document.getElementById("2").className = "main-image";
+            document.getElementById(sideImage).style.display = "";
+            sideImage = 7;
+            mainImage = 2;
         break;
         case 3:
-            document.getElementById("3").className = "main-image"
+            document.getElementById(mainImage).style.display = "none";
+            document.getElementById("3").className = "main-image";
+            document.getElementById(sideImage).style.display = "";
+            sideImage = 8;
+            mainImage = 3;
         break;
         case 4:
-            document.getElementById("4").className = "main-image"
+            document.getElementById(mainImage).style.display = "none";
+            document.getElementById("4").className = "main-image";
+            document.getElementById(sideImage).style.display = "";
+            sideImage = 9;
+            mainImage = 4;
+        break;
+        case 5:
+            document.getElementById(mainImage).style.display = "none";
+            document.getElementById("5").className = "main-image";
+            document.getElementById(sideImage).style.display = "";
+            sideImage = 5;
+            mainImage = 0;
+        break;
+        case 6:
+            document.getElementById(mainImage).style.display = "none";
+            document.getElementById("6").className = "main-image";
+            document.getElementById(sideImage).style.display = "";
+            sideImage = 6;
+            mainImage = 1;
+        break;
+        case 7:
+            document.getElementById(mainImage).style.display = "none";
+            document.getElementById("7").className = "main-image";
+            document.getElementById(sideImage).style.display = "";
+            sideImage = 7;
+            mainImage = 2;
+        break;
+        case 8:
+            document.getElementById(mainImage).style.display = "none";
+            document.getElementById("8").className = "main-image";
+            document.getElementById(sideImage).style.display = "";
+            sideImage = 8;
+            mainImage = 3;
+        break;
+        case 9:
+            document.getElementById(mainImage).style.display = "none";
+            document.getElementById("9").className = "main-image";
+            document.getElementById(sideImage).style.display = "";
+            sideImage = 9;
+            mainImage = 4;
         break;
     }
 }
