4d737c7edf95491687f20e90a6567a044e272bf3
Timo-GD <omit400@gmail.com>
2025-09-30 10:49:45 +0200

    added an image slideshow

--- src/app.js
+++ src/app.js
@@ -1,70 +1,34 @@
-let mainImage = 0;
-let sideImage = 5
-function switchImage(n) {
-    switch (n)
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slides");
+    let selectors = document.getElementsByClassName("bottom-selector");
+    if(n > slides.length)
+        slideIndex = 1;
+    else if(n < 1)
+        slideIndex = slides.length;
+
+    for(let i = 0; i < slides.length; i++)
     {
-        case 1:
-            document.getElementById(mainImage).style.display = "none";
-            document.getElementById("1").className = "main-image";
-            document.getElementById(sideImage).style.display = "";
-            sideImage = 6;
-            mainImage = 1;
-        break;
-        case 2:
-            document.getElementById(mainImage).style.display = "none";
-            document.getElementById("2").className = "main-image";
-            document.getElementById(sideImage).style.display = "";
-            sideImage = 7;
-            mainImage = 2;
-        break;
-        case 3:
-            document.getElementById(mainImage).style.display = "none";
-            document.getElementById("3").className = "main-image";
-            document.getElementById(sideImage).style.display = "";
-            sideImage = 8;
-            mainImage = 3;
-        break;
-        case 4:
-            document.getElementById(mainImage).style.display = "none";
-            document.getElementById("4").className = "main-image";
-            document.getElementById(sideImage).style.display = "";
-            sideImage = 9;
-            mainImage = 4;
-        break;
-        case 5:
-            document.getElementById(mainImage).style.display = "none";
-            document.getElementById("5").className = "main-image";
-            document.getElementById(sideImage).style.display = "";
-            sideImage = 5;
-            mainImage = 0;
-        break;
-        case 6:
-            document.getElementById(mainImage).style.display = "none";
-            document.getElementById("6").className = "main-image";
-            document.getElementById(sideImage).style.display = "";
-            sideImage = 6;
-            mainImage = 1;
-        break;
-        case 7:
-            document.getElementById(mainImage).style.display = "none";
-            document.getElementById("7").className = "main-image";
-            document.getElementById(sideImage).style.display = "";
-            sideImage = 7;
-            mainImage = 2;
-        break;
-        case 8:
-            document.getElementById(mainImage).style.display = "none";
-            document.getElementById("8").className = "main-image";
-            document.getElementById(sideImage).style.display = "";
-            sideImage = 8;
-            mainImage = 3;
-        break;
-        case 9:
-            document.getElementById(mainImage).style.display = "none";
-            document.getElementById("9").className = "main-image";
-            document.getElementById(sideImage).style.display = "";
-            sideImage = 9;
-            mainImage = 4;
-        break;
+        slides[i].style.display = "none";
     }
+
+    for(let i = 0; i < selectors.length; i++)
+    {
+        if(i == (slideIndex - 1))
+            selectors[i].style.display = "none";
+        else
+            selectors[i].style.display = "initial";
+    }
+
+    slides[slideIndex-1].style.display = "initial";
 }
